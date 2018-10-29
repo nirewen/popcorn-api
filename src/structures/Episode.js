7e895421927f3adbf9ed5f4db3321b460d437f8a
nirewen <eduardolonderovieira@gmail.com>
2018-10-29 14:29:18 -0200

    Remove TVEpisode and AnimeEpisode

--- src/structures/Episode.js
+++ src/structures/Episode.js
@@ -1,6 +1,14 @@
+import TorrentList from 'structures/TorrentList';
+
 export default class Episode {
     constructor(data) {
-        this.id = data._id;
         this.title = data.title;
+        this.episode = data.episode;
+        this.season = data.season;
+        this.tvdb_id = data.tvdb_id;
+        this.overview = data.overview;
+        this.first_aired = data.first_aired;
+        this.date_based = data.date_based;
+        this.torrents = new TorrentList(data.torrents);
     }
 }
