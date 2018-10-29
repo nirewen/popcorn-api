c3936c00a836b5fd0de94bcca03535d968fd1900
nirewen <eduardolonderovieira@gmail.com>
2018-10-29 13:36:14 -0300

    Fix tabs

--- src/PopCorn.js
+++ src/PopCorn.js
@@ -5,37 +5,41 @@ import Show from 'structures/Show';
 import * as Constants from 'core/Constants';
 
 export default class PopCorn {
-    static async search({tab = 'shows'} = {}) {
+    static async search({tab} = {}) {
         let result = await fetch(Constants.page(...arguments)).then(r => r.json());
 
         return result.map(res => {
             return {
-                movies: new Movie(res),
-                shows: new Show(res),
-                animes: new Anime(res)
-            }[tab];
+                movies: () => new Movie(res),
+                shows: () => new Show(res),
+                animes: () => new Anime(res)
+            }[tab]();
         });
     }
 
-    static async pages(tab = 'shows') {
-        let result = await fetch(Constants.pages({tab})).then(r => r.json());
+    static async pages(tab) {
+        let result = await fetch(Constants.pages(tab)).then(r => r.json());
 
         return result.length;
     }
 
-    static async details(id) {
-        let result = await fetch(Constants.details({id})).then(r => r.json());
-
-        return new Show(result);
-    }
-
-    static async random(tab = 'show') {
-        let result = await fetch(Constants.random({tab})).then(r => r.json());
+    static async details({tab, id} = {}) {
+        let result = await fetch(Constants.details({tab, id})).then(r => r.json());
 
         return {
-            movies: new Movie(result),
-            shows: new Show(result),
-            animes: new Anime(result)
-        }[tab];
+            movie: () => new Movie(result),
+            show: () => new Show(result).fetch(),
+            anime: () => new Anime(result).fetch()
+        }[tab]();
+    }
+
+    static async random(tab) {
+        let result = await fetch(Constants.random(tab)).then(r => r.json());
+
+        return {
+            movie: () => new Movie(result),
+            show: () => new Show(result),
+            anime: () => new Anime(result)
+        }[tab]();
     }
 }
