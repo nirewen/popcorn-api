dab02b711b4cbf82bd5d6e746a0bf795f7bb3731
nirewen <eduardolonderovieira@gmail.com>
2018-10-29 13:36:31 -0300

    Fix constants

--- src/core/Constants.js
+++ src/core/Constants.js
@@ -5,4 +5,4 @@ const BASE = 'https://tv-v2.api-fetch.website';
 export const pages = (tab = 'shows') => `${BASE}/${tab}`;
 export const page = ({tab = 'shows', page = 1, sort = 'trending', order = -1, genre = 'all', query} = {}) => `${BASE}/${tab}/${page}?${qs.stringify({sort, order, genre, keywords: query})}`;
 export const details = ({id, tab = 'show'} = {}) => `${BASE}/${tab}/${id}`;
-export const random = ({tab = 'show'} = {}) => `${BASE}/random/${tab}`;
+export const random = (tab = 'show') => `${BASE}/random/${tab}`;
