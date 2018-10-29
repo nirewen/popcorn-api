3ec83b56c00641f695d4db2d4b9dc4ef182f5d64
nirewen <eduardolonderovieira@gmail.com>
2018-10-29 13:34:45 -0300

    Add more examples

--- examples/example.js
+++ examples/example.js
@@ -18,3 +18,15 @@ PopCorn.search({tab: 'movies', query: 'darko'})
     .then(async ([movie]) => {
         console.log(movie);
     });
+
+// * Get the number of pages of a tab
+PopCorn.pages()
+    .then(console.log);
+
+// * Get details of a movie from ID 'tt2250912'
+PopCorn.details({tab: 'movie', id: 'tt2250912'})
+    .then(console.log);
+
+// * Get a random result from 'anime' tab
+PopCorn.random('anime')
+    .then(console.log);
