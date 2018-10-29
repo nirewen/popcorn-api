9c09af076f89d70945e5891e2c2f35ad23bce70c
nirewen <eduardolonderovieira@gmail.com>
2018-10-29 12:59:22 -0200

    Add more examples

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,20 @@
 const PopCorn = require('../');
 
-PopCorn.search({keywords: 'flash'})
-    .then(async ([show]) => {
-        await show.fetch();
+// * Search for Animes using query "akame"
+PopCorn.search({tab: 'animes', query: 'akame'})
+    .then(async ([anime]) => {
+        await anime.fetch();
 
-        console.log(show.seasons[0].episodes[0].title); // Pilot
+        console.log(anime);
+    });
+
+// * Search for TV Shows using query "flash"
+PopCorn.search({tab: 'shows', query: 'flash'})
+    .then(([show]) => show.fetch())
+    .then(console.log);
+
+// * Search for Movies using query "darko"
+PopCorn.search({tab: 'movies', query: 'darko'})
+    .then(async ([movie]) => {
+        console.log(movie);
     });
