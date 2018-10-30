00b16ac51f595df4c4b28ce8ef3fd3bd1c5fcf26
nirewen <eduardolonderovieira@gmail.com>
2018-10-30 13:02:01 -0300

    Fix deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,6 +4,7 @@ set -e
 
 if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Not building for a non master branch push - building without deploying."
+  npm run build
   npm run docs
   exit 0
 fi
@@ -14,13 +15,14 @@ REPO=$(git config remote.origin.url)
 SHA=$(git rev-parse --verify HEAD)
 
 TARGET_BRANCH="gh-pages"
-git clone $REPO dist -b $TARGET_BRANCH
+git clone $REPO distr -b $TARGET_BRANCH
 
+npm run build
 npm run docs
 
-rsync -vau docs/ dist/
+rsync -vau docs/ distr/
 
-cd dist
+cd distr
 git add --all .
 git config user.name "Travis CI"
 git config user.email "${COMMIT_EMAIL}"
