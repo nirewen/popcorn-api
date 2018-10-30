210a9dc8ac749d5afc14646b685c53c991b199a3
nirewen <eduardolonderovieira@gmail.com>
2018-10-30 08:30:59 -0300

    Document everything

--- src/PopCorn.js
+++ src/PopCorn.js
@@ -7,15 +7,36 @@ const ShowController = new RouteController({ dataClass: Show, tab: 'show' });
 const MovieController = new RouteController({ dataClass: Movie, tab: 'movie' });
 const AnimeController = new RouteController({ dataClass: Anime, tab: 'anime' });
 
+/**
+ * The PopCorn class
+ * @module PopCorn
+ * @class
+ */
 export default class PopCorn {
+
+    /**
+     * Get the shows route
+     * @returns {RouteController}
+     * @readonly
+     */
     static get shows() {
         return ShowController;
     }
 
+    /**
+     * Get the movies route
+     * @returns {RouteController}
+     * @readonly
+     */
     static get movies() {
         return MovieController;
     }
 
+    /**
+     * Get the animes route
+     * @returns {RouteController}
+     * @readonly
+     */
     static get animes() {
         return AnimeController;
     }
