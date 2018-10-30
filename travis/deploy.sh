32ed38317e96da23d8f2ff8e8b8f2969bbb24206
nirewen <eduardolonderovieira@gmail.com>
2018-10-30 09:38:12 -0300

    Redo travis config

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
+set -e
+
+if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Not building for a non master branch push - building without deploying."
+  npm run docs
+  exit 0
+fi
+
+echo -e "Building for a master branch push - building and deploying."
+
+REPO=$(git config remote.origin.url)
+SHA=$(git rev-parse --verify HEAD)
+
 TARGET_BRANCH="gh-pages"
+git clone $REPO dist -b $TARGET_BRANCH
 
-function docGen {
-  npm run-script docs
-}
-NODE_VERSION=`node --version`
-NODE_VERSION=${NODE_VERSION:1:1}
+npm run docs
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    exit 0
-fi
-
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-git clone $REPO docs
-cd docs
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-rm -rf docs/**/* || exit 0
-
-docGen
-
-cd docs
+cd dist
+git add --all .
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-git push $SSH_REPO $TARGET_BRANCH
+git config user.email "${COMMIT_EMAIL}"
+git commit -m "Docs build: ${SHA}" || true
+git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
